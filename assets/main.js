e7896d78255b027c0037dcddf584bf146feee0bc
Raiicon <orankai@gmail.com>
2018-11-07 00:30:18 -0500

    final commit of night zzz

--- assets/main.js
+++ assets/main.js
@@ -1,10 +1,20 @@
-var wordGuessDBZ = new Object();
-wordGuessDBZ.wins = 0;
-wordGuessDBZ.loses = 0;
-wordGuessDBZ.gameStateOn = false;
-wordGuessDBZ.guessesLeft = 0;
-wordGuessDBZ.lettersGuessed = "";
-wordGuessDBZ.dbzArray = ["son goku","bulma","master roshi","krillin","piccolo","son gohan","tenshinhan"];
+var wordGuess = new Object();
+wordGuess.wins = 0;
+wordGuess.loses = 0;
+wordGuess.gameStateOn = false;
+wordGuess.guessesLeft = 0;
+wordGuess.lettersGuessed = "";
+wordGuess.Array = ["son goku","bulma","master roshi","krillin","piccolo","son gohan","tenshinhan"];
+
+var randWord = wordGuess.Array[Math.floor(Math.random() * wordGuess.Array.length)];
+var gresult = wgGame(gresult){
+for(var i; i < randWord.length; i++) {
+    // if ()
+}
+}
+// wordGuess.onkeypress = gameOn() {
+//     wordGuess.gameStateOn = true;
 
 
+// };
 
