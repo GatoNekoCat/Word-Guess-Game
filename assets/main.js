c89b7da287a0ac3f83da6ed3e703319ec50c90bf
Raiicon <orankai@gmail.com>
2018-11-07 17:36:00 -0500

    changed wordGuess var to gameState for clarification

--- assets/main.js
+++ assets/main.js
@@ -1,28 +1,43 @@
 
-var randWord = wordGuess.wordBankArray[Math.floor(Math.random() * wordGuess.Array.length)];
-
-var wordGuess = {
-wins: 0,
-gameStateOn: false,
-guessesLeft: 0,
-lettersGuessed: "",
-wordBankArray: [
-    "son goku","bulma","master roshi",
-    "krillin","piccolo","son gohan","tenshinhan"
-    ],
-
-
-// var gameWord: wordChooser: function() {
-
-var randWord = this.wordBankArray[Math.floor(Math.random() * wordGuess.Array.length)];
+// gameState Object
+    var gameState = {
+        wins: 0,
+        gameStateOn: false,
+        guessesLeft: 0,
+        lettersGuessed: "",
+        wordBankArray: [
+            "son goku","bulma","master roshi",
+            "krillin","piccolo","son gohan","tenshinhan", "namek", "freiza",
+            "bardock", "vegeta",
+            ],
+        
 
 
-}
+    // Choose a random word
+    chooseRandomWord: function(wordsArray) {
+        var randWord = gameState.wordBankArray[Math.floor(Math.random() * gameState.wordBankArray.length)];
+
+        return randWord;
+    },
+    word: gameState.chooseRandomWord(gameState.wordBankArray),
+    // blankSpaceForLetters: function(word) {
+        
+    // }
+
+
+
+// end of gameState object
 };
-// wordGuess.onkeypress = gameOn() {
-//     wordGuess.gameStateOn = true;
+
+var word = gameState.chooseRandomWord(gameState.wordBankArray) ;
+
+console.log(word);
+
+
+// gameState.onkeypress = gameOn() {
+//     gameState.gameStateOn = true;
 
 
 // };
 
-// wordGuess.onkeypress = function(){myScript};
+// gameState.onkeypress = function(){myScript};
